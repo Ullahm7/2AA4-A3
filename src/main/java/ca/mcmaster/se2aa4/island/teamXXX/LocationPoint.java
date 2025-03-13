e7ba13964b2738faddc42aa40b83555abdc7e2c4
Muhammad Ullah <muhullah25@gmail.com>
2025-03-13 14:40:27 -0700

    Added location point when scanning

---
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/LocationPoint.java
@@ -0,0 +1,14 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+public class LocationPoint {
+    
+    private int x;
+    private int y; 
+    boolean isGround;
+    boolean isPOI;
+
+    public LocationPoint(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
