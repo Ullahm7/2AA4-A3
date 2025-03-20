722dd01c3023a76639e26529654388ea0c26ec3d
Muhammad Ullah <muhullah25@gmail.com>
2025-03-20 09:37:28 -0700

    Drone turning

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Drone.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Drone.java
@@ -6,10 +6,65 @@ public class Drone {
     InterestPoints direction;
     LocationPoint currentLocation;
     Heading currentHeading;
+    Heading initialHeading;
 
+    //decision making variables:
+    String action;
+    String decision;
+
+    //enums of heading directions 
     public enum Heading {
         N, E, S, W
     }
+
+    //methods of turning directions:
+    //this turns the drone left depending on currentHeading
+    public Heading leftSide(Heading currentHeading) {
+        switch (currentHeading) {
+            case N:
+                return Heading.W;
+            case E:
+                return Heading.N;
+            case S:
+                return Heading.E;
+            case W:
+                return Heading.S;
+            default:
+                throw new IllegalArgumentException("Invalid heading: " + currentHeading);
+        }
+    }
+
+    public Heading rightSide(Heading currentHeading) {
+        switch (currentHeading) {
+            case N:
+                return Heading.E;
+            case E:
+                return Heading.S;
+            case S:
+                return Heading.W;
+            case W:
+                return Heading.N;
+            default:
+                throw new IllegalArgumentException("Invalid heading: " + currentHeading);
+        }
+    }
+
+    public Heading backSide(Heading currentHeading) {
+        switch (currentHeading) {
+            case N:
+                return Heading.S;
+            case E:
+                return Heading.W;
+            case S:
+                return Heading.N;
+            case W:
+                return Heading.E;
+            default:
+                throw new IllegalArgumentException("Invalid heading: " + currentHeading);
+        }
+    }
+
+
 
     public Drone(Integer batteryLevel, String currentHeading, MapRepresenter map) {
         this.batteryLevel = batteryLevel;
