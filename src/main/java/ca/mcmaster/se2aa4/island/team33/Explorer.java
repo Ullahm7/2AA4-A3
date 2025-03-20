bca25163c8a31b29a92e87a5236b9fc23cc82aae
_tvd <talha.atif.89@gmail.com>
2025-03-20 16:30:50 -0400

    Work from old repo

--- src/main/java/ca/mcmaster/se2aa4/island/team33/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team33/Explorer.java
@@ -7,26 +7,38 @@ import org.apache.logging.log4j.Logger;
 import eu.ace_design.island.bot.IExplorerRaid;
 import org.json.JSONObject;
 import org.json.JSONTokener;
+import org.json.JSONArray;
 
 public class Explorer implements IExplorerRaid {
 
     private final Logger logger = LogManager.getLogger();
+
+    private JSONObject checker = new JSONObject(); // to store values to check for iteration
+
+    Drone drone;
+    MapRepresenter map;
+    Control control;
 
     @Override
     public void initialize(String s) {
         logger.info("** Initializing the Exploration Command Center");
         JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
         logger.info("** Initialization info:\n {}",info.toString(2));
+
         String direction = info.getString("heading");
         Integer batteryLevel = info.getInt("budget");
+
+        map = new MapRepresenter();
+        drone = new Drone(batteryLevel, direction, map);
+        control = new Control(drone, map);
+
         logger.info("The drone is facing {}", direction);
         logger.info("Battery level is {}", batteryLevel);
     }
 
     @Override
     public String takeDecision() {
-        JSONObject decision = new JSONObject();
-        decision.put("action", "stop"); // we stop the exploration immediately
+        String decision = control.nextDecision();
         logger.info("** Decision: {}",decision.toString());
         return decision.toString();
     }
@@ -34,6 +46,7 @@ public class Explorer implements IExplorerRaid {
     @Override
     public void acknowledgeResults(String s) {
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
+        
         logger.info("** Response received:\n"+response.toString(2));
         Integer cost = response.getInt("cost");
         logger.info("The cost of the action was {}", cost);
@@ -47,5 +60,11 @@ public class Explorer implements IExplorerRaid {
     public String deliverFinalReport() {
         return "no creek found";
     }
-
+    public static void main(String[] args) {
+        Explorer e = new Explorer();
+        e.initialize("{\"budget\":1000,\"heading\":\"N\"}");
+        e.takeDecision();
+        e.acknowledgeResults("{\"cost\":1,\"status\":\"success\",\"extras\":{\"range\":1}}");
+        e.deliverFinalReport();
+    }
 }
---
+++ src/main/java/ca/mcmaster/se2aa4/island/team33/InterestPoints.java
@@ -0,0 +1,45 @@
+package ca.mcmaster.se2aa4.island.teamXXX;
+
+import java.util.ArrayList;
+
+/*looking at _pois.json we get info on this: 
+ * "uid": "1f3f5c9f-8a64-4ab1-bd1a-c8fa705fec56",
+        "kind": "Creek",
+        "x": 560.247106714227,
+        "y": 765.8454474411957
+*/
+
+public class InterestPoints {
+
+    private String identifier;
+    private String kind;
+    private double[] location; //contains x and y
+
+    public InterestPoints(String identifier, String type, double[] location){
+        this.identifier = identifier;
+        this.kind = kind;
+        this.location = location;
+    }
+
+    public String getId(){
+        return null;
+    }
+
+    public String getKind(){
+        return null;
+    }
+
+    public double[] getLocation(){
+        return null;
+    }
+    
+    public void setIdentifier(String identifier){
+        
+    }
+   
+    public ArrayList<String> getInfo(){
+        return null;
+
+    }
+    
+}
