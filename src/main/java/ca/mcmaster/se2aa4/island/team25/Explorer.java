48bfb49db3b5fdc237e2dcb29ea7b12845cd6f13
_tvd <talha.atif.89@gmail.com>
2025-03-21 00:09:07 -0400

    Initial Setup

--- src/main/java/ca/mcmaster/se2aa4/island/team25/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team25/Explorer.java
@@ -1,13 +1,13 @@
 package ca.mcmaster.se2aa4.island.team25;
 
 import java.io.StringReader;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import eu.ace_design.island.bot.IExplorerRaid;
 import org.json.JSONObject;
 import org.json.JSONTokener;
-import org.json.JSONArray;
+
+import eu.ace_design.island.bot.IExplorerRaid;
 
 public class Explorer implements IExplorerRaid {
 
@@ -23,14 +23,10 @@ public class Explorer implements IExplorerRaid {
     public void initialize(String s) {
         logger.info("** Initializing the Exploration Command Center");
         JSONObject info = new JSONObject(new JSONTokener(new StringReader(s)));
-        logger.info("** Initialization info:\n {}",info.toString(2));
+        logger.info("** Initialization info:\n {}", info.toString(2));
 
         String direction = info.getString("heading");
         Integer batteryLevel = info.getInt("budget");
-
-        map = new MapRepresenter();
-        drone = new Drone(batteryLevel, direction, map);
-        control = new Control(drone, map);
 
         logger.info("The drone is facing {}", direction);
         logger.info("Battery level is {}", batteryLevel);
@@ -38,16 +34,17 @@ public class Explorer implements IExplorerRaid {
 
     @Override
     public String takeDecision() {
-        String decision = control.nextDecision();
-        logger.info("** Decision: {}",decision.toString());
+        JSONObject decision = new JSONObject();
+        decision.put("action", "stop"); // we stop the exploration immediately
+        logger.info("** Decision: {}", decision.toString());
         return decision.toString();
     }
 
     @Override
     public void acknowledgeResults(String s) {
         JSONObject response = new JSONObject(new JSONTokener(new StringReader(s)));
-        
-        logger.info("** Response received:\n"+response.toString(2));
+
+        logger.info("** Response received:\n" + response.toString(2));
         Integer cost = response.getInt("cost");
         logger.info("The cost of the action was {}", cost);
         String status = response.getString("status");
@@ -60,6 +57,7 @@ public class Explorer implements IExplorerRaid {
     public String deliverFinalReport() {
         return "no creek found";
     }
+
     public static void main(String[] args) {
         Explorer e = new Explorer();
         e.initialize("{\"budget\":1000,\"heading\":\"N\"}");
