047d86b3e64eda6426cda980d213cff5271aaaa4
Huzayl Yusuf-zai <huzayl.yusufzai@gmail.com>
2025-03-22 13:07:38 -0400

    Added body of Coordinate class, initializing, get and set (Closes #4)

--- src/main/java/ca/mcmaster/se2aa4/island/team25/Coordinate.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team25/Coordinate.java
@@ -1,5 +1,27 @@
 package ca.mcmaster.se2aa4.island.team25;
 
 public class Coordinate {
+    private int x,y; //X and Y coordinates 
+
+    public coodinate (int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+
+    public void setY(int y){
+        this.y = y;
+    }
 
 }
