006e7c070144278dc570a8755b284eeca7c6e8ad
Muhammad Ullah <muhullah25@gmail.com>
2025-03-22 09:54:43 -0700

    storage handling

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Storage.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Storage.java
@@ -1,5 +1,71 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 import java.util.List;
 public class Storage {
-    
+    private Integer range;
+    private Integer cost;
+    private String found;
+    private List<String> creeks;
+    private List<String> biomes;
+    private String site;
+
+    public Storage(){
+
+    }
+
+    public void setRange(Integer range){
+        this.range = range;
+    }
+
+    public void setCost(Integer cost){
+        this.cost = cost;
+    }
+
+    public void setFound(String found){
+        this.found = found;
+    }
+
+    public void setBiomes(List<String> biomes){
+        this.biomes = biomes;
+    }
+
+    public void setCreeks(List<String> creeks){
+        this.creeks = creeks;
+    }
+
+    public void setSite(String site){
+        this.site = site;
+    }
+
+    public Integer getRange(){
+        return this.range;
+    }
+
+    public Integer getCost(){
+        return this.cost;
+    }
+
+    public String getFound(){
+        return this.found;
+    }
+
+    public List<String> getBiomes(){
+        return this.biomes;
+    }
+
+    public List<String> getCreeks(){
+        return this.creeks;
+    }
+
+    public String getSite(){
+        return this.site;
+    }
+
+    public void clear(){
+        this.range = -1; //needs to before 0
+        this.cost = null;
+        this.found = "null";
+        this.biomes = null;
+        this.creeks = null;
+        this.site = null;
+    }
 }
