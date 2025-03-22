d350136f8fd466859e921c2da3761d2409d53988
_tvd <talha.atif.89@gmail.com>
2025-03-22 18:55:58 -0400

    Added Kind ENUM

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team25/CoastSearch.java
@@ -0,0 +1,19 @@
+package ca.mcmaster.se2aa4.island.team25;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
+public class CoastSearch implements SearchMethod {
+
+    private final Logger logger = LogManager.getLogger();
+
+    public JSONObject nextStep() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void giveInfo(JSONObject info) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
