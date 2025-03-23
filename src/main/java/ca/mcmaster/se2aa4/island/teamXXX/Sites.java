3906001e17ef8cd916339e7ecfa0b09eb5c90267
Muhammad Ullah <muhullah25@gmail.com>
2025-03-23 11:30:17 -0700

    adding extensions of interest points of locations of creeks and sites

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Sites.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Sites.java
@@ -1,5 +1,16 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 
-public class Sites {
+public class Sites extends InterestPoints {
+
+    public Sites(InterestPoints poi) {
+        super(poi);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void storeScanResults(Storage scanResults) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'storeScanResults'");
+    }
     
 }
