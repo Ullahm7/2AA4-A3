87c695b155a4a7db20cc98e1f47334f13c1f2ac7
Muhammad Ullah <muhullah25@gmail.com>
2025-03-23 11:26:54 -0700

    added points that implements the location points

--- src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Points.java
+++ src/main/java/ca/mcmaster/se2aa4/island/teamXXX/Points.java
@@ -1,49 +1,60 @@
 package ca.mcmaster.se2aa4.island.teamXXX;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Points implements LocationPoint {
 
-    @Override
+    private int rowNumber;
+    private int columnNumber;
+    private boolean isGround = false;
+    private boolean beenScanned = false;
+
+    private List<String> biomes = new ArrayList<>();
+
+    public Points(int rowNumber, int columnNumber) {
+        this.rowNumber = rowNumber;
+        this.columnNumber = columnNumber;
+    }
+
     public int getRow() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getRow'");
+        return rowNumber;
     }
 
-    @Override
     public int getColumn() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getColumn'");
+        return columnNumber;
     }
 
-    @Override
-    public void addBiomes(List<String> biomes) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addBiomes'");
-    }
-
-    @Override
     public Boolean getGround() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getGround'");
+        if (biomes.size() == 0) {
+            isGround = false;
+            return isGround;
+        }
+        if (biomes.size() == 1 && biomes.get(0).equals("OCEAN")) {
+            isGround = false;
+        } else {
+            isGround = true;
+        }
+        return isGround;
     }
 
-    @Override
-    public void storeScanResults(Storage scanResults) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'storeScanResults'");
-    }
-
-    @Override
     public boolean getBeenScanned() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getBeenScanned'");
+        return beenScanned;
+    }
+    public void setBeenScanned(boolean beenScanned) {
+        this.beenScanned = beenScanned;
     }
 
-    @Override
-    public void setBeenScanned(boolean beenScanned) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setBeenScanned'");
+    public void addBiomes(List<String> biome) {
+        for (String b : biome) {
+            this.biomes.add(b);
+        }
+    }
+
+    public void storeScanResults(Storage scanResults) {
+        beenScanned = true;
+        addBiomes(scanResults.getBiomes());
+
     }
     
 }
