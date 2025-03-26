0248a9c10f663d8d86876a42d5fafaa116bf9fd6
Muhammad Ullah <muhullah25@gmail.com>
2025-03-26 14:22:25 -0700

    adding @BeforeEach for initial direction of drone

--- src/test/java/ca/mcmaster/se2aa4/island/team25/DirectionTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team25/DirectionTest.java
@@ -1,8 +1,23 @@
 package ca.mcmaster.se2aa4.island.team25;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class DirectionTest {
+
+    private Direction droneCurrentDirectionNorth; 
+    private Direction droneCurrentDirectionEast; 
+    private Direction droneCurrentDirectionSouth; 
+    private Direction droneCurrentDirectionWest;
+
+    @BeforeEach
+    public void initializeDroneDirectionalFace(){
+        droneCurrentDirectionNorth = Direction.NORTH; 
+        droneCurrentDirectionEast = Direction.EAST;   
+        droneCurrentDirectionSouth = Direction.SOUTH;
+        droneCurrentDirectionWest = Direction.WEST;
+    }
     
 }
