514c75aaf5efde6bf9225a6f67ba41e135332054
Muhammad Ullah <muhullah25@gmail.com>
2025-03-29 11:05:53 -0700

    add files

---
+++ src/main/java/ca/mcmaster/se2aa4/island/team25/FullUTurn.java
@@ -0,0 +1,45 @@
+package ca.mcmaster.se2aa4.island.team25;
+
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
+public class FullUTurn implements SearchMethod {
+
+    //private final Logger logger = LogManager.getLogger();
+    private Drone drone;
+
+    private int counter = 0;
+    private boolean isNorth;
+
+    public FullUTurn(Drone drone) {
+        this.drone = drone;
+        this.isNorth = (this.drone.currentDir() == Direction.NORTH);
+    }
+
+    @Override
+    public JSONObject nextStep() {
+        //logger.info("*** STOPPING ***" + (this.counter + 1));
+        //testing
+        this.counter++;
+        if (this.counter == 1 || this.counter == 3) {
+            return this.drone.simpleAction(Action.FLY);
+        }
+
+        return this.isNorth
+                ? this.drone.turnLeft()
+                : this.drone.turnRight();
+    }
+
+    @Override
+    public SearchMethod searchType() {
+        if (drone.goHome()) {
+            return new FindHome(this.drone);
+        }
+
+        if (this.counter == 4) {
+            return new StraightLine(this.drone, true);
+        }
+        return this;
+    }
+}
